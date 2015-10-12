c55e01918bb2d3ae872a4db22b3f8a240bc9b1c3
Terry Yin <terry@odd-e.com>
2015-10-12 14:07:59 +0800

    legacy code

--- vue2048/Gruntfile.js
+++ vue2048/Gruntfile.js
@@ -1,62 +1,41 @@
 module.exports = function(grunt) {
+  'use strict';
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+  // Project configuration.
+  grunt.initConfig({
+    jasmine : {
+      src : 'js/**/*.js',
+      options : {
+        specs : 'spec/**/*.js'
+      }
+    },
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'js/**/*.js',
+        'spec/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['**/*.js'],
+        tasks: ['jshint', 'jasmine'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+  });
 
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-		concat: {
-			css: {
-				src: [
-					'css_build/*'
-				],
-				dest: 'build/css/all.css'
-			}
-		},
+  grunt.registerTask('test', ['jshint', 'jasmine']);
 
-		cssmin: {
-			css: {
-				src: 'build/css/all.css',
-				dest: 'css/all.min.css'
-			}
-		},
-
-		watch: {
-			options: { //35729
-				livereload: true,
-			},
-			css: {
-				files: ['css/*.css']
-			},
-			html: {
-				files: ['*.html']
-			},
-			js: {
-				files: ['js/*.js'],
-			}
-		},
-
-		connect: {
-			'static': {
-				options: {
-					hostname: 'localhost',
-					port: 8001
-					// base: 'www-root'
-				}
-			}
-		}
-
-	});
-
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-
-	grunt.registerTask('default', ['concat', 'cssmin', 'browserify', 'uglify']);
-	grunt.registerTask('server', ['connect:static', 'watch']);
-
-	// define an alias for common tasks
-	// grunt.registerTask('myTasks', ['task1', 'task2:target', 'task3']);		
+  grunt.registerTask('default', ['test']);
 
 };
